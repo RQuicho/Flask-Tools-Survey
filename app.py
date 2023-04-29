18e18b5d0714bb7f332008fbf3dd7b768e2a9ece
RQuicho <raymondquicho@gmail.com>
2023-04-29 16:31:04 -0700

    adding answers route

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = "my-secret-key"
+app.config['SECRET_KEY'] = "my-secret-key"
 
 responses = []
 
@@ -12,19 +12,43 @@ def home_page():
 	"""Shows home page displaying user, title of the survey, instructions, and a button to start survey."""
 	title = satisfaction_survey.title
 	instructions = satisfaction_survey.instructions
-
+	
 	return render_template('home_page.html', title=title, instructions=instructions)
 
 @app.route('/questions/<int:id>')
 def show_question(id):
 	"""Shows a form asking current question and listing choices"""
-	# sat_survey.questions[0].question
+	
 	question = satisfaction_survey.questions[id].question
 	choices = satisfaction_survey.questions[id].choices
 
-	return render_template('question.html', question=question, choices=choices)
+	return render_template('question.html', question=question, choices=choices, id=id)
 
-	
+
+@app.route('/answer', methods=["POST"])
+def show_answer():
+	"""Shows answer that the use selected."""
+	answer = request.form['answer'] #stores value of selected radio button (i.e. Yes)
+	responses.append(answer)
+
+	return redirect(f'/questions/{len(responses)}')
+
+
+
+
+
+# @app.route('/questions/<int:id>', methods=["POST"])
+# def show_answer(id):
+# 	"""Shows answer that the use selected."""
+# 	answer = request.form['answer'] #stores value of selected radio button (i.e. Yes)
+# 	responses.append(answer)
+
+# 	# return redirect('questions/<int:id> + 1')
+
+# 	return f"""
+# 		<p>your saved answer is {answer}</p>
+# 	"""
+
 	
 	
 
